793ab6273b1740666a242d754f1cb18e5e3e3b83
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2021-12-09 19:39:50 -0600

    Reformat the translations

--- src/installer_wizard_en.ts
+++ src/installer_wizard_en.ts
@@ -1,112 +1,180 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
-<context><name>WizardInstaller</name><message>
-        <location filename="installer.py" line="57" /><source>BAIN Wizard Installer</source>
-        <translation type="unfinished" />
-    </message>
-<message>
-        <location filename="installer.py" line="63" /><source>Installer for BAIN archive containing wizard scripts.</source>
-        <translation type="unfinished" />
-    </message>
-    </context><context><name>WizardInstallerComplete</name><message>
-        <location filename="ui\wizardinstallercomplete.ui" line="0" /><location filename="ui\wizardinstallercomplete.ui" line="0" /><source>The installer script has finished. The following sub-packages and plugins will be installed.</source>
-        <translation type="unfinished" />
+  <context>
+    <name>WizardInstaller</name>
+    <message>
+      <location filename="installer.py" line="57" />
+      <source>BAIN Wizard Installer</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallercomplete.ui" line="0" /><location filename="ui\wizardinstallercomplete.ui" line="0" /><source>Sub-Packages:</source>
-        <translation type="unfinished" />
+      <location filename="installer.py" line="63" />
+      <source>Installer for BAIN archive containing wizard scripts.</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>WizardInstallerComplete</name>
+    <message>
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <source>The installer script has finished. The following sub-packages and plugins will be installed.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallercomplete.ui" line="0" /><location filename="ui\wizardinstallercomplete.ui" line="0" /><source>Plugins:</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <source>Sub-Packages:</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallercomplete.ui" line="0" /><location filename="ui\wizardinstallercomplete.ui" line="0" /><source>INI Tweaks:</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <source>Plugins:</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallercomplete.ui" line="0" /><location filename="ui\wizardinstallercomplete.ui" line="0" /><source>Notes:</source>
-        <translation type="unfinished" />
-    </message>
-</context><context><name>WizardInstallerDialog</name><message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="dialog.py" line="633" /><source>Next</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <source>INI Tweaks:</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="dialog.py" line="635" /><source>Install anyway</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <location filename="ui\wizardinstallercomplete.ui" line="0" />
+      <source>Notes:</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>WizardInstallerDialog</name>
+    <message>
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="dialog.py" line="633" />
+      <source>Next</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="dialog.py" line="639" /><source>Install</source>
-        <translation type="unfinished" />
+      <location filename="dialog.py" line="635" />
+      <source>Install anyway</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>BAIN Wizard Installer</source>
-        <translation type="unfinished" />
+      <location filename="dialog.py" line="639" />
+      <source>Install</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>Name</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>BAIN Wizard Installer</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>Manual</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>Name</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>Script</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>Manual</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>Back</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>Script</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerdialog.ui" line="0" /><location filename="ui\wizardinstallerdialog.ui" line="0" /><source>Cancel</source>
-        <translation type="unfinished" />
-    </message>
-</context><context><name>WizardInstallerPage</name><message>
-        <location filename="ui\wizardinstallerpage.ui" line="0" /><location filename="ui\wizardinstallerpage.ui" line="0" /><source>Options:</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>Back</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerpage.ui" line="0" /><location filename="ui\wizardinstallerpage.ui" line="0" /><source>Description:</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <location filename="ui\wizardinstallerdialog.ui" line="0" />
+      <source>Cancel</source>
+      <translation type="unfinished" />
     </message>
-</context><context><name>WizardInstallerRequires</name><message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=" font-weight:600;"&gt;Warning:&lt;/span&gt; The following version requirements are not met for using this installer.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished" />
+  </context>
+  <context>
+    <name>WizardInstallerPage</name>
+    <message>
+      <location filename="ui\wizardinstallerpage.ui" line="0" />
+      <location filename="ui\wizardinstallerpage.ui" line="0" />
+      <source>Options:</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Requirements</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerpage.ui" line="0" />
+      <location filename="ui\wizardinstallerpage.ui" line="0" />
+      <source>Description:</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>WizardInstallerRequires</name>
+    <message>
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=" font-weight:600;"&gt;Warning:&lt;/span&gt; The following version requirements are not met for using this installer.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Graphics Extender</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Requirements</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Wrye Bash</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Graphics Extender</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Need</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Wrye Bash</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Game</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Need</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>N/A</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Game</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Have</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>N/A</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="ui\wizardinstallerrequires.ui" line="0" /><location filename="ui\wizardinstallerrequires.ui" line="0" /><source>Script Extender</source>
-        <translation type="unfinished" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Have</source>
+      <translation type="unfinished" />
     </message>
-</context></TS>
+    <message>
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <location filename="ui\wizardinstallerrequires.ui" line="0" />
+      <source>Script Extender</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+</TS>
